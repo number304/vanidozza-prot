15aef5fcaebee2c7f314b7bedb9d2b56ef9600c6
number304 <jmpmateo3042013@gmail.com>
2021-06-24 23:18:01 -0500

    basic composition of prototype done

---
+++ src/components/BookingMachine.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container class="mt-4">
+    <v-card max-width="360" class="text-center mx-auto pa-4">
+
+      <h3 class="text-h4 font-weight-medium">Book a service</h3>
+      <p class="text-overline">Your new look awaits you</p>
+
+      <v-form v-model="isValid">
+
+        <v-text-field
+          v-model="clientName"
+          label="Your name"
+          :rules="rules"
+        ></v-text-field>
+        <v-select :items="services" label="services" v-model="selectedService" :rules="rules"></v-select>
+        <v-select :items="extras" label="optional extras" v-model="selectedExtra"></v-select>
+        <date-picker
+          type="datetime"
+          v-model="bookingDate"
+          format="ddd D MMM, hh:mm a"
+          :time-picker-options="timeOptions"
+        />
+        <v-card-actions class="d-flex justify-center">
+          <v-btn class="px-4" :disabled="!isValid">Confirm</v-btn>
+        </v-card-actions>
+
+      </v-form>
+
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/index.css'
+
+export default {
+  components: { DatePicker },
+  data() {
+    return {
+      isValid: false,
+      clientName: '',
+      services: ['Haircut', 'Dye', 'Keratin Treatment', 'Hair straightener'],
+      extras: ['Extra 1', 'Extra Attention', 'Extra 3', 'Premium Kelp Shampoo'],
+      selectedService: null,
+      selectedExtra: null,
+      bookingDate: null,
+      timeOptions: {
+        start: '08:30',
+        step: '00:30',
+        end: '18:30',
+        format: 'ddd, hh:mm a'
+      },
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'At least 3 characters.',
+      ],
+    }
+  },
+}
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar
+    app
+  >
+  <h2 class="mx-auto" style="color: black">Your local haircut saloon</h2>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
